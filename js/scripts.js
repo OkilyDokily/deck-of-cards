75618b1f895dc88505996f5564c17ef2cad690b5
me <me@me.me.com>
2020-04-22 15:27:58 -0700

    finish project up

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 var suits = ["clubs","diamonds","hearts","spades"];
 var highCards = ["jack", "queen", "king"]
-var ace = "ace"
+var ace = ["ace"];
 var numbers = Array.from(Array(11).keys()).slice(2);
-
+console.log(numbers)
 function go(){
   var cards = [];
-  var sequence = ace.concat(number).concat(highCards);
+  var sequence = ace.concat(numbers).concat(highCards);
+  console.log(sequence);
   suits.forEach(function(suit){
     sequence.forEach(function(cardValue){
-
+      var string = cardValue + " of " + suit;
+      cards.push(string); 
     });
   })
   return cards;
@@ -17,6 +19,6 @@ function go(){
 
 $(document).ready(function(){
   go().forEach(function(card){
-    $()
+    $("ul").append("<li>" + card +"</li>")
   });
 });
